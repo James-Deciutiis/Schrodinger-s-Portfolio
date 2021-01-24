ffa8ba6c21e8a98d7f0ecaf760c93569435208e7
JamesDeciutiis <46979777+James-Deciutiis@users.noreply.github.com>
2021-01-24 09:54:15 -0800

    final

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const { IEXCloudClient } = require("node-iex-cloud")
 const fetch = require("node-fetch")
 const express = require('express')
 const app = express()
-const PORT = 4000
 
 var balance = 0
 var starting_balance = 0
@@ -126,8 +125,3 @@ app.post('/result', function(req, res){
     }
 
 });
-
-app
-    .listen(PORT, ()=>{
-})
-
